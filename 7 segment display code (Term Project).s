e3ce596225f52a64e2f6be774503f8a8d09ed8a1
Fatehpaul Dhaliwal <dhali131@WIN.ecf.utoronto.ca>
2016-11-22 16:36:09 -0500

    Writing to all displays
    Got it working to all displays, now just need to get the right colour on
    the board

--- 7 segment display code (Term Project).s
+++ 7 segment display code (Term Project).s
@@ -1,31 +1,36 @@
-  .equ ADDR_7SEG1, 0xFF200020
-  .equ ADDR_7SEG2, 0xFF200030
+	.text /* executable code follows */
+	.global main
 
-  .global main
- 
- main:
-  movia r2,ADDR_7SEG1
-  movia r3,0xEF   # bits 0000110 will activate segments 1 and 2 
-  stwio r3,0(r2)        # Write to 7-seg display 
-  movia r2,ADDR_7SEG2
-  stwio r0, 0(r2)
-  
-  
- /* Store the 7-segment patterns starting at memory address 0x200 */
-PATTERNS:
-.byte  0x3f           		/* 0 */
-.byte  0x06           		/* 1 */
-.byte  0x5b           		/* 2 */
-.byte  0x4f           		/* 3 */
-.byte  0x66           		/* 4 */
-.byte  0x6d           		/* 5 */
-.byte  0x7d           		/* 6 */
-.byte  0x07           		/* 7 */
-.byte  0xff           		/* 8 */
-.byte  0x6f           		/* 9 */
-.byte  0x77           		/* A */
-.byte  0xfc           		/* B */
-.byte  0x39           		/* C */
-.byte  0x5e           		/* D */
-.byte  0xf9           		/* E */
-.byte  0xf1           		/* F */
+main:
+	/* initialize base addresses of parallel ports */
+	movia r20, 0xFF200020 /* HEX3_HEX0 base address */
+	movia r21, 0xFF200030 /* HEX6-HEX4 base address */
+	movia r19, HEX_bits
+	ldwio r16, 0(r19) /* load pattern for HEX3-0 displays */
+	ldwio r17, 0(r19) /* load pattern for HEX6-4 displays */
+
+DO_DISPLAY:
+	stwio r16, 0(r20) /* store to HEX3 ... HEX0 */
+	stwio r17, 0(r21) /* store to HEX6 ... HEX4 */
+	slli r6, r6, 8 /* print first read in value and then shift to the left */
+
+.data
+
+HEX_bits:
+	.byte  0x3f           		/* 0 */
+	.byte  0x06           		/* 1 */
+	.byte  0x5b           		/* 2 */
+	.byte  0x4f           		/* 3 */
+	.byte  0x66           		/* 4 */
+	.byte  0x6d           		/* 5 */
+	.byte  0x7d           		/* 6 */
+	.byte  0x07           		/* 7 */
+	.byte  0xff           		/* 8 */
+	.byte  0x6f           		/* 9 */
+	.byte  0x77           		/* A */
+	.byte  0xfc           		/* B */
+	.byte  0x39           		/* C */
+	.byte  0x5e           		/* D */
+	.byte  0xf9           		/* E */
+	.byte  0xf1           		/* F */
+	.end
